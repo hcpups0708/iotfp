35d7bdbae1e13b4b9c95e1331c7dcbe656fba826
Asus <hcpups0708@gmail.com>
2014-12-28 13:37:53 +0800

    webrtc first commit

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var express = require('express')
 var app = express()
+var webRTC = require('webrtc.io').listen(8001);
 
-//app.use(express.staticProvider(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 //app.set('root',"C:/Users/Asus/Documents/交大/4上/物連網/iotfp");
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.sendFile('C:/Users/Asus/Documents/交大/4上/物連網/iotfp/public/index.html');
-})
+})*/
 
-var server = app.listen(4321, function () {
+var server = app.listen(80, function () {
 
   var host = server.address().address
   var port = server.address().port
