7cda5a9fa2a48597a48b51ea07cdcb0f8fdb9f45
Chalos <cyl_05019@hotmail.com>
2014-12-29 20:01:59 +0800

    fuck error

--- api/public/javascript/Radar.js
+++ api/public/javascript/Radar.js
@@ -37,8 +37,8 @@ Radar.renderData = function(canvas, data) {
     	var _v = v * (canvas.height/2)/150;
     	if(_v < 15) _v = 15;
 		return [
-			Math.cos(i*10*Math.PI / 180) * _v + canvas.width/2,
-			Math.sin(i*10*Math.PI / 180) * _v + canvas.height/2
+			Math.cos(i*20*Math.PI / 180) * _v + canvas.width/2,
+			Math.sin(i*20*Math.PI / 180) * _v + canvas.height/2
 		];
 	}).reduce(function(pre, curr) {
 		return pre.concat(curr);
