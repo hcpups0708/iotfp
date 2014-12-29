617a926ddeadaff930063c118cb1d065c31b5ba3
Chalos <cyl_05019@hotmail.com>
2014-12-29 20:46:22 +0800

    radar using pizza

--- api/public/javascript/Radar.js
+++ api/public/javascript/Radar.js
@@ -10,6 +10,7 @@ Radar.drawBase = function() {
 	ctx.fill();
 
 	var radius = [150, 120, 90, 60, 30];
+	ctx.lineWidth = 2;
 	radius.forEach(function(value, index) {
 		var _value = value * (canvas.height/2)/150;
 		ctx.beginPath();
@@ -33,36 +34,16 @@ Radar.renderData = function(canvas, data) {
 	var ctx = canvas.getContext('2d');
 	ctx.clearRect(0 ,0 , canvas.width, canvas.height);
 
-    var array = data.map(function(v, i){
-    	var _v = v * (canvas.height/2)/150;
-    	if(_v < 15) _v = 15;
-		return [
-			Math.cos(i*20*Math.PI / 180) * _v + canvas.width/2,
-			Math.sin(i*20*Math.PI / 180) * _v + canvas.height/2
-		];
-	}).reduce(function(pre, curr) {
-		return pre.concat(curr);
-	});
-
-	array.length = data.length * 2;
-	ctx.strokeStyle = '#66CCFF';
-	ctx.lineWidth = 3;
-	ctx.beginPath();
-    ctx.moveTo(array[0], array[1]);
-    for(var i=2; i< array.length-1; i+=2) {
-    	if((array[i-2] !== canvas.width/2 || array[i+1-2] !== canvas.height/2) 
-    		&& (array[i] !== canvas.width/2 || array[i+1] !== canvas.height/2)) {
-    		ctx.lineTo(array[i], array[i+1]);
-    	} else {
-        	ctx.moveTo(array[i], array[i+1]);
-        }
-    }
-    ctx.closePath();
-    ctx.stroke();
-    ctx.fillStyle = 'red';
-    for(var i=0; i<array.length; i+=2) {
-        ctx.fillRect(array[i]-2.5, array[i+1]-2.5, 5, 5);
-    }
+    ctx.fillStyle = 'green';
+    data.forEach(function(v,i){
+		var x = canvas.width/2;
+		var y = canvas.height/2;
+		var r = v * (canvas.height/2)/150;
+		var iInit = (36 - 2 * i  - 1) * 10 * Math.PI / 180;
+		var iFinal = (36 - 2 * i + 1) * 10 * Math.PI / 180;
+		ctx.arc(x,y,r,iInit,iFinal);
+		ctx.fill();
+    });
 }
 
 Radar.init = function(_w) {
@@ -76,13 +57,13 @@ Radar.init = function(_w) {
 	R.setAttribute('id', 'rBase');
 	R.setAttribute('height', rSize);
 	R.setAttribute('width', rSize);
-	R.setAttribute('style', 'z-index:2;position:absolute;opacity:0.7;');
+	R.setAttribute('style', 'z-index:2;position:absolute;opacity:1;');
 
 	var R2 = document.createElement('canvas');
 	R2.setAttribute('id', 'rData');
 	R2.setAttribute('height', rSize);
 	R2.setAttribute('width', rSize);
-	R2.setAttribute('style', 'z-index:3;position:absolute;');
+	R2.setAttribute('style', 'z-index:3;position:absolute;opacity:0.8;');
 
 	radar.appendChild(R);
 	radar.appendChild(R2);
