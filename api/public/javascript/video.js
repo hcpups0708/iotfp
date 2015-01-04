5cc5e909e25954d1fe269683d1f4ad8b95b48a75
Chalos <cyl_05019@hotmail.com>
2015-01-05 01:11:47 +0800

    add container for video

--- api/public/javascript/video.js
+++ api/public/javascript/video.js
@@ -2,9 +2,14 @@
 var rtcOrigin = 'chalos2.nctucs.net:8081';
 
 var VideoStart = function() {
+	var div = document.createElement('div');
+	div.setAttribute('style', 'overflow:hidden;display:block;width:100%;height:100%;');
+
 	var video = document.createElement('video');
 	video.setAttribute('id', 'video');
-	document.body.appendChild(video);
+
+	div.appendChild(video);
+	document.body.appendChild(div);
 
 	startRtc();
 }
