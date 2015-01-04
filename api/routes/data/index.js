a5b091400a9f264428ea9574cffc53047a267f8f
Chalos <cyl_05019@hotmail.com>
2015-01-05 00:03:24 +0800

    push

--- api/routes/data/index.js
+++ api/routes/data/index.js
@@ -1,12 +1,31 @@
 var GSeter = require('../../lib/GSeter');
 
+var radarSize = 18;
 var data = {};
 
 data.pressure = new GSeter();
 data.humidility = new GSeter();
 data.temperature = new GSeter();
-data.radar = new GSeter(18);
+data.radar = new GSeter(radarSize);
 data.compass = new GSeter();
 data.movement = new GSeter();
+
+data.updateRadar = function(initial, data1, data2) {
+	console.log(arguments);
+	var increase = (initial >= 0)? true : false;
+	var size = data1.length || data2.length;
+	initial = Math.abs(initial);
+
+	for(var i=0; i<size; i++) {
+		var indexToChange = (increase)? i+initial: i-initial;
+		if(indexToChange <= 0 || indexToChange >=8 ) increase = !increase;
+
+		console.log(indexToChange);
+		//console.log(increase);
+
+		data.radar.set(indexToChange, Number(data1[i]));
+		data.radar.set(indexToChange+9, Number(data2[i]));
+	}
+}
 
 module.exports = data;
--- api/routes/index.js
+++ api/routes/index.js
@@ -4,32 +4,6 @@ var express = require('express');
 var router = express.Router();
 var data = require('./data');
 
-var ref = {
-	p: 'pressure', h: 'humidility', t: 'temperature'
-};
-
-router.get('/p', function(req, res, next) {
-	res.send({result:data.pressure.get()});
-});
-
-router.get('/h', function(req, res, next) {
-	res.send({result:data.humidility.get()});
-});
-
-router.get('/t', function(req, res, next) {
-	res.send({result:data.temperature.get()});
-});
-
-router.get('/c', function(req, res, next) {
-	res.send({result:data.compass.get()});
-});
-
-router.get('/r/:index', function(req, res, next) {
-	var index = req.params.index;
-	if(index === 'all') res.send({result:data.radar.get()});
-	else res.send({result:data.radar.get(Number(index))});
-});
-
 router.get('/move', function(req, res, next) {
 	var result = data.movement.get() || 's';
 	//console.log(result);
@@ -46,37 +20,6 @@ router.get('/getAll', function(req, res, next) {
 	});
 });
 
-router.post('/p/:value', function(req, res, next) {
-	var value = req.params.value;
-	data.pressure.set(Number(value));
-	res.end();
-});
-
-router.post('/h/:value', function(req, res, next) {
-	var value = req.params.value;
-	data.humidility.set(Number(value));
-	res.end();
-});
-
-router.post('/t/:value', function(req, res, next) {
-	var value = req.params.value;
-	data.temperature.set(Number(value));
-	res.end();
-});
-
-router.post('/r/:index/:value', function(req, res, next) {
-	var index = Math.floor(Number(req.params.index)/2);
-	var value = req.params.value;
-	if(value !== 0) data.radar.set(index, Number(value));
-	res.end();
-});
-
-router.post('/c/:value', function(req, res, next) {
-	var value = req.params.value;
-	data.compass.set(Number(value));
-	res.end();
-});
-
 router.post('/move/:direct', function(req, res, next) {
 	var direct = req.params.direct;
 	//console.log(direct);
@@ -85,30 +28,18 @@ router.post('/move/:direct', function(req, res, next) {
 });
 
 router.post('/update', function(req, res, next) {
-	var params = req.query;
-	console.log(req.body);
+	var params = req.body;
+	console.log(params);
 	data.temperature.set(Number(params.t));
 	data.humidility.set(Number(params.h));
 	data.pressure.set(Number(params.p));
 	data.compass.set(Number(params.c));
 
 	var index = Math.floor(Number(params.r)/2);
-	if(Number(params.d1) !== 0) data.radar.set(index, Number(params.d1));
-	if(Number(params.d2) !== 0) data.radar.set(index+9, Number(params.d2));
+	//if(Number(params.d1) !== 0) data.radar.set(index, Number(params.d1));
+	//if(Number(params.d2) !== 0) data.radar.set(index+9, Number(params.d2));
+	data.updateRadar(index, params.d1, params.d2);
 	res.end();
-});
-
-router.post('/setDirectGetAll/:direct', function(req, res, next) {
-	var direct = req.params.direct;
-	console.log(direct);
-	data.movement.set(direct);
-	res.send({
-		h: data.humidility.get(),
-		t: data.temperature.get(),
-		p: data.pressure.get(),
-		c: data.compass.get(),
-		r: data.radar.get()
-	});
 });
 
 module.exports = router;
