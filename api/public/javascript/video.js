0ee4eea388a8825399fd88ec94e6b92a0b907642
Chalos <cyl_05019@hotmail.com>
2015-01-05 12:08:09 +0800

    overflow hidden

--- api/public/javascript/video.js
+++ api/public/javascript/video.js
@@ -2,14 +2,15 @@
 var rtcOrigin = 'chalos2.nctucs.net:8081';
 
 var VideoStart = function() {
-	var div = document.createElement('div');
-	div.setAttribute('style', 'overflow:hidden;display:block;width:100%;height:100%;');
+	//var div = document.createElement('div');
+	//div.setAttribute('style', 'overflow:hidden;display:block;width:100%;height:100%;');
 
 	var video = document.createElement('video');
 	video.setAttribute('id', 'video');
 
-	div.appendChild(video);
-	document.body.appendChild(div);
+	//div.appendChild(video);
+	//document.body.appendChild(div);
+	document.body.appendChild(video);
 
 	startRtc();
 }
